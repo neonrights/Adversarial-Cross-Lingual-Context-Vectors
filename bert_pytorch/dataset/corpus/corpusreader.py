2ffb4e0024793c2f0f9279ce7ec9228fff943213
neonrights <nicholasfarn@gmail.com>
2018-11-02 21:01:57 -0700

    parent class for corpus documents readers

---
+++ bert_pytorch/dataset/corpus/corpusreader.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import zipfile
+import tarfile
+
+
+class BadCorpusError(Exception):
+	pass
+
+
+class CorpusReader:
+	"""
+	Parent class for all corpus readers.
+	Assumes each file in a corpus corresponds to a single document.
+	Can take a directory or compressed file as input.
+	"""
+
+	def __init__(self, corpus_path):
+		if os.isdir(corpus_path):
+			self.corpus_type = 'dir'
+			self.corpus = corpus_path
+			self.files = [file for _, _, files in os.walk(corpus_path) for file in files]
+		elif zipfile.is_zipfile(filename):
+			self.corpus_type = 'zip'
+			self.corpus = zipfile.ZipFile(corpus_path, 'r')
+			self.files = [file for file in self.corpus.namelist() if file[-1] != '/'] # not directory
+		elif tarfile.is_tarfile(filename):
+			self.corpus_type = 'tar'
+			self.corpus = tarfile.open(corpus_path, 'r:gz')
+			self.files = self.corpus.getnames()
+		else:
+			raise BadCorpusError("corpus \"{}\" was not a directory, zip file, or tarball".format(corpus_path)) 
+
+	def extract_sentences(self, file):
+		# reads file and returns a generator yielding next sentence
+		raise NotImplementedError
+
+	def __len__(self):
+		return len(self.files)
+
+	def __getitem__(self, index):
+		return self.sentence_extractor(self.files[index])
+
+	def __iter__(self):
+		return iter(self)
+
+	def close(self):
+		if self.corpus_type in {'zip', 'tar'}:
+			self.corpus.close()
+

063696ae7bf87cb30cd18a9899ea48d64b2ce7e3
neonrights <nicholasfarn@gmail.com>
2018-11-28 13:35:57 -0800

    wrote unittest for MT transformer model, added combined masking

--- crosslingual_bert/model/__init__.py
+++ crosslingual_bert/model/__init__.py
@@ -1,3 +1,4 @@
 from .multilingual_bert import MultilingualBERT
 from .bert_official import BertConfig, BertModel
 from .simple_adversary import SimpleAdversary
+from .translator import TranslatorModel

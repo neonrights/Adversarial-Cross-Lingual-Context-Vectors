e7065062f8842c0137a0f8ac6405dd11a4b1e70e
neonrights <nicholasfarn@gmail.com>
2018-11-28 13:44:47 -0800

    added tests for combined masks

--- crosslingual_bert/test_translator.py
+++ crosslingual_bert/test_translator.py
@@ -1,11 +1,12 @@
 import torch
 
 from model import TranslatorModel, BertConfig
+import pdb
 
 print("Starting unittests for translator model...")
 
 vocab_size = 10000
-seq_len = 20
+seq_len = 4
 batch_size = 5
 
 config = BertConfig(vocab_size=vocab_size)
@@ -14,7 +15,20 @@ print("passed initialization test")
 
 test_encoder = torch.rand(batch_size, seq_len, config.hidden_size)
 test_ids = torch.randint(high=vocab_size, size=(batch_size, seq_len), dtype=torch.long)
-test_layers, test_pool = model(test_encoder, test_ids)
+"""test_layers, test_pool = model(test_encoder, test_ids)
+
+assert len(test_layers) == config.num_hidden_layers
+for layer in test_layers:
+	assert layer.shape == (batch_size, seq_len, config.hidden_size)
+
+assert test_pool.shape == (batch_size, config.hidden_size)"""
+
+print("passed forward propagation tests")
+
+encoder_mask = torch.randint(high=2, size=(batch_size, seq_len), dtype=torch.long)
+id_mask = torch.randint(high=2, size=(batch_size, seq_len), dtype=torch.long)
+
+test_layers, test_pool = model(test_encoder, test_ids, encoder_mask, id_mask)
 
 assert len(test_layers) == config.num_hidden_layers
 for layer in test_layers:
@@ -22,6 +36,6 @@ for layer in test_layers:
 
 assert test_pool.shape == (batch_size, config.hidden_size)
 
-print("passed forward propagation tests")
+print("passed masking tests")
 
 print("passed all tests")

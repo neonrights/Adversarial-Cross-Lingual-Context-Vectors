b8bc7ec0c7d11fe9e7091cb426dd0b94129296eb
neonrights <nicholasfarn@gmail.com>
2019-02-19 18:43:36 -0800

    updated optimizers to use adafactor

--- crosslingual_bert/trainer/adversarial_pretrain.py
+++ crosslingual_bert/trainer/adversarial_pretrain.py
@@ -6,11 +6,10 @@ import torch
 import torch.nn as nn
 
 from itertools import chain
-from torch.optim import *
 from torch.utils.data import DataLoader
 from apex.parallel import DistributedDataParallel
 
-from .optimization import *
+from .optimization import Adafactor
 from .utils import *
 
 import tqdm
@@ -218,11 +217,11 @@ class AdversarialPretrainer:
 
         # initialize optimizers
         if parallelize:
-            self.D_optim = Adadelta(self.model.module.component_parameters("adversary"), config.lr)
-            self.lm_optims = Adadelta(self.model.module.component_parameters(), config.lr)
+            self.D_optim = Adafactor(self.model.module.component_parameters("adversary"), config.lr)
+            self.lm_optims = Adafactor(self.model.module.component_parameters(), config.lr)
         else:
-            self.D_optim = Adadelta(self.model.component_parameters("adversary"), config.lr) # adversary optimizer
-            self.lm_optims = Adadelta(self.model.component_parameters(), config.lr)
+            self.D_optim = Adafactor(self.model.component_parameters("adversary"), config.lr) # adversary optimizer
+            self.lm_optims = Adafactor(self.model.component_parameters(), config.lr)
 
         # hyperparameters for loss
         self.beta = config.beta
